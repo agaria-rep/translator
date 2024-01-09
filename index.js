75747667ecd6b3c1aa1cc6eb0952559e72242247
Agaria <96198356+agaria-rep@users.noreply.github.com>
2024-01-09 19:06:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ let params = new URLSearchParams(document.location.search);
 var from_t = "RUS".toUpperCase(); // params.get("from").toUpperCase();
 var to_t = params.get("to").toUpperCase();
 
-document.querySelectorAll(".type-from").forEach((e) => {e.innerText = from_t});
-document.querySelectorAll(".type-to").forEach((e) => {e.innerText = to_t});
+if (to_t == "" || ( to_t != "EEE" && to_t != "FYA")) {
+  to_t = `<select onclick="toChoose()"><option value="" disabled>Choose</option><option value="eee">EEE</option><option value="fya">FYA</option></select>`;
+}
+
+document.querySelector(".type-from").innerText = from_t;
+document.querySelector(".type-to").innerText = to_t;
 document.querySelector("title").innerText = to_t+" Translator";
+
+
+function toChoose() {
+  window.location.href += "s";
+}
+
+alert(window.location.href);
