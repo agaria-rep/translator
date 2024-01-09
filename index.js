2ac7c5f765717d5ce74c8fff00103101c8375f6a
Agaria <96198356+agaria-rep@users.noreply.github.com>
2024-01-09 18:57:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+let params = new URLSearchParams(document.location.search);
 
+var from_t = "RUS".toUpperCase(); // params.get("from").toUpperCase();
+var to_t = params.get("to").toUpperCase();
+
+document.querySelectorAll(".type-from").forEach((e) => {e.innerText = from_t});
+document.querySelectorAll(".type-to").forEach((e) => {e.innerText = to_t});
+document.querySelector("title").innerText = to_t+" Translator";
